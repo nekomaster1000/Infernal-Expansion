6521f71c33c5bcc7a7faec0bd001c5f460b290ee
BaconKami <natou022@gmail.com>
2020-08-06 20:33:29 -0400

    Added fabric files

---
+++ src/main/java/com/baconkami/infernalexp/InfernalExpansion.java
@@ -0,0 +1,12 @@
+package com.baconkami.infernalexp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InfernalExpansion  implements ModInitializer {
+    public static final String MOD_ID = "infernalexp";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
